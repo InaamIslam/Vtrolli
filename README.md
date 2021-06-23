0d2855684ce938f0e173cc64bced96aae5c032b0
Inaam Islam <Inaam.islaam@gmail.com>
2021-06-23 16:10:25 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my_project_wk5
+My Project. To create a CRUD application with utilisation of supporting tools, methodologies and technologies that encapsulate all core modules covered during training.
---
+++ app/routes.py
@@ -0,0 +1,39 @@
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# @app.route('/home')
+# def home():
+#     return 'WELCOME TO VTROLLI'
+
+# @app.route('/shop')
+# def shop():
+#     return 'Select your items from the list of products below, then select your preferred collection time.'
+
+# @app.route('/checkout')
+# def checkout():
+#     return 'Your order details are below. Please confirm and process.'
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
