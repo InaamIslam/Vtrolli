5de22db24f376cd3edd491e1a30b9146945d7d7e
Inaam Islam <Inaam.islaam@gmail.com>
2021-06-26 17:50:17 +0000

    final update saturday

--- vtrolli/application/routes.py
+++ vtrolli/application/routes.py
@@ -25,43 +25,30 @@ def add():
             collection_date_time = form.collection_date_time.data)
         db.session.add(new_order)
         db.session.commit()
-        #query for the new order and then pass that through to the checkout
-        confirmation_order = orders.query.get(new_order)
-        return redirect(url_for('checkout', confirmation_order = new_order)) #add the info to database 
+        return redirect(url_for('checkout', confirmation_order = new_order)) 
     return render_template('shop.html', form=form)
 
-#Should returnthe checkout page with a list of the most recent order with a confirm button 
-
-@app.route('/checkout/<id>', methods=['GET', 'POST'])
-def confirm(id):    #read the order placed on shop
+@app.route('/checkout/<id>')
+def confirm(id):   
     my_order = orders.query.filter_by(id=id).first()
     return render_template('checkout.html', myorder = my_order, form=form)
-       #if customer is happy with order, they confirm, if not can update or delete
-#     #second form for updating here
-#     #two buttons update and delete 
-#     #delete button for delete order
-    
+        
 
 @app.route('/update/<id>', methods=['GET', 'POST'])  
-def update_order(id):
-    all_orders = order_form.query.all()
-    my_order_update= my_order
-    db.session.commit()
-
-
-#     #This route allows for a form to be filled in again to change the order
-#     #only the product changes in this case
-   
-@app.route('/delete/<id>', methods=['GET', 'POST']) 
-delete_order(id_num):
-        all_orders = order_form.query.all()
-        my_order_delete= all_orders.query.get(id_num)
-        db.session.delete(my_order_delete)
+def update(id):
+    form = order_form
+    order_update = order_form.query.get(id)
+    if form.validate_on_submit():
+        order_update.product = form.product.data
         db.session.commit()
- #redirect to shop URL
- #doesnt need a render template
- #query database and delete object then return to @route shop
+        return redirect(url_for('home'))
+    return render_template('update.html', myorder = my_order, form=form)
 
+@app.route('/delete/<id>')
+def delete(id):
+    order_delete = orders.query.get(id)
+    db.session.delete(order_delete)
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
