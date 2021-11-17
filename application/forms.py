d0da5bb00a8fd57ddf0151ee87b24ab6a627a925
Inaam Islam <Inaam.islaam@gmail.com>
2021-11-17 16:33:37 +0000

    chnages to forms

--- application/forms.py
+++ application/forms.py
@@ -6,24 +6,31 @@ from wtforms.validators import DataRequired, ValidationError
 
 from application.models import orders, products
 
+class trolley_check:
+    def __init__(self,message):
+        self.message = message
+
+    def __call__(self, form, field):
+        all_series = trolley.query.all()
+        for trolley in all_trolley:
+            if series.name == field.data:
+                raise ValidationError(self.message)
     
-class order_form(FlaskForm):
-    first_name = StringField('First name:', validators = [DataRequired(),
-        ] )
+class trolley_form(FlaskForm):
+    name = StringField('Trolley name', 
+		validators=[
+			DataRequired(),
+			SeriesCheck(message='This trolley name already exists')])
+    submit = SubmitField('Create Trolley')
 
-    last_name = StringField('Last name:', validators = [DataRequired(),
-        ] )
-
-    email = StringField('Email:', validators = [DataRequired(),
-        ] )
-
-    product = StringField( 'My chosen product is:', validators = [DataRequired(),
-        ] )
-
-    collection_date_time = StringField('Please state your preferred date and time for collection:', validators = [DataRequired(),
-        ] )
-
-    submit = SubmitField('PLACE ORDER')
+class products_form(FlaskForm):
+    desc = StringField('Product',
+		validators=[
+			DataRequired()])
+    rating = SelectField('Ratings',
+		choices=[
+			('5', '5/5'),('4','4/5'),('3','3/5'),('2','2/5'),('1','1/5'), ('0','0/5')])
+    submit = SubmitField('Add Review')
 
 
     
